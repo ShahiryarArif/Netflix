0e22f3ecb7daf1aa0f3cac9a7400676b9d7ada16
Shahiryar Arif <shahiryararif@Shahiryars-MacBook-Air.local>
2025-05-14 04:08:48 +0500

    play button added

--- netflix-clone/components/BillBoard.tsx
+++ netflix-clone/components/BillBoard.tsx
@@ -1,6 +1,7 @@
 "use client";
 import useBillboard from "@/hooks/useBillboard";
 import { AiOutlineInfoCircle } from "react-icons/ai"
+import PlayButton from "./PlayButton";
 
 const Billboard = () => {
   const { data } = useBillboard();
@@ -24,6 +25,7 @@ const Billboard = () => {
           {data?.description}
         </p>
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+          <PlayButton movieId={data?.id} />
           <button className="bg-white/30 text-white rounded-md py-1 md:py-2 px-2 md:px-4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-white/20 transition">
             <AiOutlineInfoCircle className="mr-1"/>
             More Info
